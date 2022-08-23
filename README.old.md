a884479916ff3f8ea9ba1f86fd243c96428b031f
CrimsonMax <61148415+CrimsonMax@users.noreply.github.com>
2022-08-23 23:19:05 +0300

    Initialization

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# react-authorization
+ typescript + redux
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { FC } from 'react';
+import './App.css';
+
+export const App: FC = () => {
+  return (
+    <div>
+      Hello!
+    </div>
+  )
+}
